0fd4b57b351c652ad5b8843c84d6e9e379224b71
jalala-pv <99414691+jalala-pv@users.noreply.github.com>
2024-10-01 07:28:30 +0100

    incdec.py

---
+++ incdec.py
@@ -0,0 +1,6 @@
+5)to input a no then increment by5 and decrement it by 3
+num=int(input("eneter  number"))
+increment=num+5
+print("incremented value by 5 is",increment)
+decrement=increment-3
+print("decremented value by 3 is",decrement)
