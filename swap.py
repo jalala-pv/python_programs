91a15b193313dc51192e21f1c2b025671688ac92
jalala-pv <99414691+jalala-pv@users.noreply.github.com>
2024-10-01 07:17:47 +0100

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,10 @@
+num1=int(input("eneter  number1"))
+num2=int(input("eneter  number2"))
+print("entered number1 is",num1)
+print("entered number2 is",num2)
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("after swaping")
+print("value of number 1 is",num1)
+print("value of number 2 is",num2)
