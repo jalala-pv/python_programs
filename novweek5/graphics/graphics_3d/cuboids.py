8f74429bacd68faca2410b34a475938017b49732
jalala-pv <99414691+jalala-pv@users.noreply.github.com>
2024-11-29 10:47:10 +0000

    Create cuboids.py

---
+++ novweek5/graphics/graphics_3d/cuboids.py
@@ -0,0 +1,6 @@
+def cuboidarea(length, breadth, height):
+    return 2 * (length * breadth + breadth * height + height * length)
+def cuboidperimeter(length, breadth, height):
+    return 4 * (length + breadth + height)
+
+
